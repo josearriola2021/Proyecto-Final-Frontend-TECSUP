3f1de0b02c422d6e96f3bc23ab715cfa001cc610
josearriola2021 <joseluisperez1718@gmail.com>
2023-06-22 10:16:36 -0500

    commit config

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                    <Route path="/profile/:user" element={<ProfileView />} />
+                <Route path="/profile/:user" element={<ProfileView />} />
                 <Route element={<FooterLayout/>}>
                     <Route path="/" element={<PrincipalView />} />
                     <Route path="/:nombre/:id" element={<ProductosInfoView />} />
